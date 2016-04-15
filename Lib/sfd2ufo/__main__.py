aba17b13811eb6bcb3dd5c9d20cd10cbcfc68f00
Khaled Hosny <khaledhosny@eglug.org>
2016-04-15 18:39:26 +0200

    Use argparse

--- Lib/sfd2ufo/__main__.py
+++ Lib/sfd2ufo/__main__.py
@@ -1,5 +1,16 @@
-import sys
+import argparse
+
 from . import SFDFont
 
-font = SFDFont(sys.argv[1])
-font.save(sys.argv[2])
+def main():
+    parser = argparse.ArgumentParser(description="Convert FontForge fonts to UFO.")
+    parser.add_argument("sfdfile", metavar="FILE", help="input font to process")
+    parser.add_argument("ufofile", metavar="FILE", help="output font to write")
+
+    args = parser.parse_args()
+
+    font = SFDFont(args.sfdfile)
+    font.save(args.ufofile)
+
+if __name__ == "__main__":
+    main()
