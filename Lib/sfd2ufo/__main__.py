f6c399677d058e1da6d2f14532f0065336da6ee6
Khaled Hosny <khaledhosny@eglug.org>
2018-01-07 19:49:09 +0200

    Set program name

--- Lib/sfd2ufo/__main__.py
+++ Lib/sfd2ufo/__main__.py
@@ -3,7 +3,8 @@ import argparse
 from . import SFDFont
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert FontForge fonts to UFO.")
+    parser = argparse.ArgumentParser(
+        prog="sfd2ufo", description="Convert FontForge fonts to UFO.")
     parser.add_argument("sfdfile", metavar="FILE", help="input font to process")
     parser.add_argument("ufofile", metavar="FILE", help="output font to write")
     parser.add_argument("--ignore-uvs", action="store_true",
