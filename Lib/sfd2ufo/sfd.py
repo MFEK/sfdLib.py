e0e25897fd583b905e6623b6e04b44f7cf06d75c
Khaled Hosny <khaledhosny@eglug.org>
2018-01-09 00:00:36 +0200

    Parse SFNT Names
    Turns out that FontForge’s UTF-7 is non-standard so I had to copy
    FontForge implementation for decoding UTF-7 strings and translate it
    from C to Python. Pretty ugly code, might make it more idiomatic Python
    later.

--- Lib/sfd2ufo/sfd.py
+++ Lib/sfd2ufo/sfd.py
@@ -2,14 +2,122 @@ import os
 import re
 
 from datetime import datetime
+
 from . import parseVersion
 
 
 LAYER_RE = re.compile('(.)\s+(.)\s+(".*?.")\s+(.?)')
+QUOTED_LIST_RE = re.compile('(".*?")')
+
+
+_INBASE64 = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+]
+
+def _SFDReadUTF7(data):
+    out = b""
+
+    if data and not isinstance(data[0], int): # Python 2
+        data = [ord(c) for c in data]
+
+    prev_cnt = 0
+    prev = 0
+
+    ch1 = 0
+    ch2 = 0
+    ch3 = 0
+    ch4 = 0
+
+    i = 0
+    inside = False
+    while i < len(data):
+        ch1 = data[i]
+        i += 1
+
+        done = False
+
+        if not done and not inside:
+            if ch1 == ord("+"):
+                ch1 = data[i]
+                i += 1
+                if ch1 == ord("-"):
+                    ch1 = ord("+")
+                    done = True
+                else:
+                    inside = True
+                    prev_cnt = 0
+            else:
+                done = True
+
+        if not done:
+            if ch1 == ord("-"):
+                inside = False
+            elif _INBASE64[ch1] == -1:
+                inside = False
+                done = True
+            else:
+                ch1 = _INBASE64[ch1]
+                ch2 = _INBASE64[data[i]]
+                i += 1
+                if ch2 == -1:
+                    i -= 1
+                    ch2 = 0
+                    ch3 = 0
+                    ch4 = 0
+                else:
+                    ch3 = _INBASE64[data[i]]
+                    i += 1
+                    if ch3 == -1:
+                        i -= 1
+                        ch3 = 0
+                        ch4 = 0
+                    else:
+                        ch4 = _INBASE64[data[i]]
+                        i += 1
+                        if ch4 == -1:
+                            i -= 1
+                            ch4 = 0
+
+                ch1 = (ch1 << 18) | (ch2 << 12) | (ch3 << 6) | ch4
+
+                if prev_cnt == 0:
+                    prev = ch1 & 0xff
+                    ch1 >>= 8
+                    prev_cnt = 1
+                else:
+                    ch1 |= (prev << 24)
+                    prev = (ch1 & 0xffff)
+                    ch1 = (ch1 >> 16) & 0xffff
+                    prev_cnt = 2
+                done = True
+
+        if done:
+            out += chr(ch1).encode()
+        if prev_cnt == 2:
+            prev_cnt = 0
+            if prev != 0:
+                out += chr(prev).encode()
+
+    return out.decode("utf-8")
 
 
 def _sfdUTF7(string):
-    return string.strip('"').encode("ascii").decode("utf-7")
+    return _SFDReadUTF7(string.strip('"').encode("ascii"))#.decode("utf-7")
 
 
 def _parsePrivateDict(font, data):
@@ -59,6 +167,46 @@ def _parsePrivateDict(font, data):
         if StdVW in info.postscriptStemSnapV:
             info.postscriptStemSnapV.pop(info.postscriptStemSnapV.index(StdVW))
         info.postscriptStemSnapV.insert(0, StdVW)
+
+_NAMES = [
+    "copyright",
+    None, # styleMapFamily
+    None, # styleMapStyle
+    "openTypeNameUniqueID",
+    None, # styleMapFamily and styleMapStyle
+    "openTypeNameVersion",
+    "postscriptFontName",
+    "trademark",
+    "openTypeNameManufacturer",
+    "openTypeNameDesigner",
+    "openTypeNameDescription",
+    "openTypeNameManufacturerURL",
+    "openTypeNameDesignerURL",
+    "openTypeNameLicense",
+    "openTypeNameLicenseURL",
+    None,
+    "openTypeNamePreferredFamilyName",
+    "openTypeNamePreferredSubfamilyName",
+    "openTypeNameCompatibleFullName",
+    "openTypeNameSampleText",
+    None,
+    "openTypeNameWWSFamilyName",
+    "openTypeNameWWSSubfamilyName",
+]
+
+def _parseNames(font, data):
+    info = font.info
+
+    langId, data = data.split(" ", 1)
+    langId = int(langId)
+    data = QUOTED_LIST_RE.findall(data)
+
+    for nameId, name in enumerate(data):
+        name = _sfdUTF7(name)
+        if name:
+            if langId == 1033: # English (United States)
+                if _NAMES[nameId]:
+                    setattr(info, _NAMES[nameId], name)
 
 
 def parse(font, path):
@@ -234,6 +382,8 @@ def parse(font, path):
             info.openTypeOS2StrikeoutPosition = int(value)
         elif key == "UniqueID":
             pass # info.XXX = value
+        elif key == "LangName":
+            _parseNames(font, value)
         elif key == "XUID":
             pass # XXX
         elif key == "UnicodeInterp":
