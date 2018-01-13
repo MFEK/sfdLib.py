457016996452b0cfb0fb73787e700f2e9287e328
Khaled Hosny <khaledhosny@eglug.org>
2018-01-13 21:15:03 +0200

    A command line option to use the native parser

--- Lib/sfd2ufo/__main__.py
+++ Lib/sfd2ufo/__main__.py
@@ -2,9 +2,6 @@ import argparse
 
 from defcon import Font
 
-from .native import SFDFont
-from .parser import SFDParser
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,14 +10,20 @@ def main():
     parser.add_argument("ufofile", metavar="FILE", help="output font to write")
     parser.add_argument("--ignore-uvs", action="store_true",
         help="don’t error if the font uses Unicode variation selectors")
+    parser.add_argument("--native", action="store_true",
+        help="use FontForge’s native module instead of our own SFD parser")
 
     args = parser.parse_args()
 
-    font = Font()
-    parser = SFDParser(args.sfdfile, font, args.ignore_uvs)
-    parser.parse()
+    if args.native:
+        from .native import SFDFont
+        font = SFDFont(args.sfdfile, args.ignore_uvs)
+    else:
+        from .parser import SFDParser
+        font = Font()
+        parser = SFDParser(args.sfdfile, font, args.ignore_uvs)
+        parser.parse()
 
-#   font = SFDFont(args.sfdfile, args.ignore_uvs)
     font.save(args.ufofile)
 
 if __name__ == "__main__":
